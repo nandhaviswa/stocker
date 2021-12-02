0730f4203b76707455b90577fd68b42ae7efd1fc
Nandha Kumar Viswanathan <nandhakumarviswanaathan@gmail.com>
2021-12-02 13:52:08 +0530

    comments updated

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const stocker = require('./stocker');
 
-stocker.type1('TEST', 10, 100, 50, 75);
-stocker.type1('NALTIONALUM', 23, 99.09, 87.15, 90);
-stocker.type1('CHALET', 3, 262.33, 226, 240);
-console.log('\n--------------------------------[ nandha ]---------------------------------\n');
-stocker.type2('TEST', 10, 100, 50, 500);
-stocker.type2('NALTIONALUM', 23, 99.09, 87.15, 6449.10);
-stocker.type2('CHALET', 3, 262.33, 226, 1130);
-console.log('\n--------------------------------[ nandha ]---------------------------------\n');
-stocker.type3('TEST', 100, 50, 500, 75);
-stocker.type3('NALTIONALUM', 99.09, 87.15, 6449.10, 90);
-stocker.type3('CHALET', 262.33, 226, 1130, 240);
-console.log('\n--------------------------------[ nandha ]---------------------------------\n');
-stocker.type4('TEST', 10, 100, 500, 75);
-stocker.type4('NALTIONALUM', 23, 99.09, 6449.10, 90);
-stocker.type4('CHALET', 3, 262.33, 1130, 240);
+var stockName = "NALTIONALUM";
+var q1 = 23;
+var p1 = 99.09;
+var p2 = 87.15;
+var p = 90;
+var l2 = 6449.10;
+
+console.log('\n--------------------------------[ type1 ]---------------------------------\n');
+// i have already bought some stocks 
+// i know the current price 
+// i want to acheive an average
+// tell me how much i need
+stocker.type1(stockName, q1, p1, p2, p);
+console.log('\n--------------------------------[ type2 ]---------------------------------\n');
+// i have already bought some stocks
+// i have a lumpsum now and i know the current price
+// tell me how much i can buy now and what will be the average
+stocker.type2(stockName, q1, p1, p2, l2);
+console.log('\n--------------------------------[ type3 ]---------------------------------\n');
+// i have a lumpsum in mind for next session
+// i want to acheive an averge with above lumpsum
+// tell me how much i can invest today
+stocker.type3(stockName, p1, p2, l2, p);
+console.log('\n--------------------------------[ type4 ]---------------------------------\n');
+// i have already bought some stocks
+// i have a lumpsum now.
+// i want to acheive the average with this lumpsum
+// tell me at what price i can trade
+stocker.type4(stockName, q1, p1, l2, p);
